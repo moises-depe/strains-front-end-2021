8b52d33230960a6fd88551df0c4d48bbd1e246eb
phsequeira <phsequeira@yahoo.com>
2021-03-09 13:10:24 -0800

    workin on search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,24 +5,45 @@ import { getAllStrains } from './UTILS/ApiUtils.js';
 
 export default class SearchPage extends Component {
     state = {
-        test: 'initial state before api call',
+        strains: {},
+        load: false,
     }
 
     componentDidMount = async () => {
         const data = await getAllStrains();
 
         this.setState({
-            test: data.body,
+            strains: Object.entries(data).slice(0, 5),
+            load: true,
         })
 
-        console.log('this shows the call made on mount')
+        console.log(this.state.strains)
     }
+
+
+
     render() {
-        console.log(this.state.test);
+        const strains = this.state.strains;
+        console.log();
         return (
             <div>
-                {this.state.test}
-            </div>
+                <form onSubmit={this.handleSubmit}>
+                <input value={this.state.search} onChange={this.handleSearchChange} />
+                <button>Search for strains</button>
+            </form>
+                {this.state.load &&
+                    strains.map((strain, i) => 
+                    <div key={i} className='strain'>
+                        <p>Name: {strain[0]}</p>
+                        <p>Type: {strain[1].race}</p>
+                        <p>Flavors: {strain[1].flavors.map(flavor => <li>{flavor}</li>)}</p>
+                        <p>Medical: {strain[1].effects.medical.map(medical => <li>{medical}</li>)}</p>
+                        <p>Positive: {strain[1].effects.positive.map(positive => <li>{positive}</li>)}</p>
+                        
+                    </div>
+                    )}
+            </div >
+            
         )
     }
 }
--- src/UTILS/ApiUtils.js
+++ src/UTILS/ApiUtils.js
@@ -1,6 +1,6 @@
 import request from 'superagent';
 
-const URL = `https://sheltered-coast-13653.herokuapp.com`;  // update with Heroku
+const URL = `https://fathomless-fortress-68501.herokuapp.com`;  // update with Heroku
 
 export async function userSignUp(email, password) {
     const response = await request
