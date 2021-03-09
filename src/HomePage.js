af70da6422f565bef0c6317362c94649cbcb683e
Casey Warren <caseywar1017@gmail.com>
2021-03-08 19:09:45 -0500

    beginning css

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import './App.css';
+import './HomePage.css';
+
+
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
+            <div className="home-main">
+                <img className="The eye" src="./medical.jpg" alt="medical symbol"></img>
                 Home
                 this is the home page yay
             </div>
