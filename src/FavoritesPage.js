dde0653c477a0dcabddcc67140469543ed33da04
Casey Warren <caseywar1017@gmail.com>
2021-03-10 18:17:55 -0500

    css changes

--- src/FavoritesPage.js
+++ src/FavoritesPage.js
@@ -3,6 +3,8 @@ import {
     getAllUserFavorites,
     deleteUserFavorite,
 } from './UTILS/ApiUtils.js';
+import './SearchPage.css';
+
 
 export default class FavoritesPage extends Component {
     state = {
@@ -31,27 +33,28 @@ export default class FavoritesPage extends Component {
     render() {
         console.log(this.state.favorites);
         return (
-            <div>
-                    <div className="favorites-wrapper">
-                        {this.state.favorites.map(favorite =>
-                            <div key={favorite.id} className="favorite-div">
-                                <h2>{favorite.name}</h2>
-                                <p>{favorite.race}</p>
-                                <p>Flavors: {favorite.flavors.join(', ')}</p>
-                                <img src={favorite.img} alt="strain" />
-                                <h3>Effects</h3>
-                                <p>Positive: {favorite.positive.join(', ')}</p>
-                                <p>Medical: {favorite.medical.join(', ')}</p>
-                                <p>Negative: {favorite.negative.join(', ')}</p>
-                                <p>{favorite.description}</p>
-                                <hr />
-                                <p>Share Favorite:</p>
-                                <p>https://competent-jones-a33e98.netlify.app/share/{favorite.id}</p>
-                                <button onClick={() => this.handleDeleteFavorite(favorite.id)}>
-                                    Delete Favorite</button>
-                            </div>
-                        )}
-                    </div>
+            <div >
+                <span className="favorites-title">Favorites</span>
+                <div className="list">
+                    {this.state.favorites.map(favorite =>
+                        <div key={favorite.id} className="strain">
+                            <p className="name">{favorite.name}</p>
+                            <p><span className="name-header">Type:</span> {favorite.race}</p>
+                            <p className="flavors"><span className="name-header">Flavors:</span> {favorite.flavors.join(', ')}</p>
+
+                            <h3>Effects</h3>
+                            <p className="recreation"><span className="name-header">Positive:</span> {favorite.positive.join(', ')}</p>
+                            <p className="medical"><span className="name-header">Medical:</span> {favorite.medical.join(', ')}</p>
+                            <p className="negative"><span className="name-header">Negative:</span> {favorite.negative.join(', ')}</p>
+                            <p>{favorite.description}</p>
+                            <hr />
+                            <p>Share Favorite:</p>
+                            <p>https://competent-jones-a33e98.netlify.app/share/{favorite.id}</p>
+                            <button onClick={() => this.handleDeleteFavorite(favorite.id)}>
+                                Delete Favorite</button>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Spinner extends Component {
+    render() {
+        return (
+            <div>
+                <img alt="loading" width="100" height="100" src="https://media.giphy.com/media/paLTRWydS53cHGXKp1/giphy.gif" />
+            </div>
+        )
+    }
+}
