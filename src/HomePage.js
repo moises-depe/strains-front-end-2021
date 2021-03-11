32ab74c51a8ce7da79bcac70187932227df08915
Casey Warren <caseywar1017@gmail.com>
2021-03-10 20:30:32 -0500

    added spinner and css

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
 import './HomePage.css';
 
+
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
-                <div className="home-main">
+            <div >
+                <div>
                     <img className="thcHome" src="./medical2.png" alt="medical symbol"></img>
                 </div>
                 <div className="home-description">
                     Strains Selector lets you search over 2000 different strains of cannabis.
-                    <br></br>
-                    Our search parameters make it easy to view the medical benefits, recreational effects, and flavors of each strain.
-                    <br></br>
-                    Strains Selector also allows you to save your favorite strains under your personal login.
-                    <br></br>
-                    A link will also be generated for each of your favorites that allows you to share the strain and all of its details.
+                        <br></br>
+                        Our search parameters make it easy to view the medical benefits, recreational effects, and flavors of each strain.
+                        <br></br>
+                        Strains Selector also allows you to save your favorite strains under your personal login.
+                        <br></br>
+                        A link will also be generated for each of your favorites that allows you to share the strain and all of its details.
                 </div>
             </div>
         )
